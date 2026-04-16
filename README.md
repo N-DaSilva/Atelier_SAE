33751da43adc496d616d86edcbb32791769c455f
N-DaSilva <158066183+N-DaSilva@users.noreply.github.com>
2026-04-16 14:32:09 +0200

    added sound effects and timer bar color change

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Atelier_SAE
+
+Error and correct Sound Effects by Universfield from Pixabay
+Tick Sound Effect by DRAGON-STUDIO from Pixabay
--- script.js
+++ script.js
@@ -12,12 +12,17 @@ document.addEventListener("DOMContentLoaded", () => {
     let rounds = 1;
     let maxRounds = 5;
 
-   
+
     const timerElement = document.getElementById("timer");
     const timerBarElement = document.getElementById("timer-bar");
-    let counter = 5 * 1000; // 5 seconds
+    
+    let counter = 6 * 1000; // 5 seconds
     const maxTime = maxRounds * 30 * 1000; // 30 secondes par tour
     let remainingTime = maxTime;
+
+    const errorSound = new Audio('medias/error.mp3');
+    const correctSound = new Audio('medias/correct.mp3');
+    const tickingSound = new Audio('medias/tick.mp3');
 
     roundsElement.textContent = `Round: ${rounds} / ${maxRounds}`;
     const updateRounds = () => {
@@ -31,8 +36,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const generateString = () => {
         let generatedString = "";
         let generatedHTML = "";
+        let randomIndex = getRandomInt(inputValues.length);
+
         for (let i = 0; i < inputValues.length; i++) {
-            generatedString += inputValues[getRandomInt(inputValues.length)];
+            if (generatedString.length > 0) {
+                while (inputValues[randomIndex] == generatedString[i-1]) {
+                    randomIndex = getRandomInt(inputValues.length);
+                }
+            }
+            generatedString += inputValues[randomIndex];
             generatedHTML += `<span id="char${i}">${generatedString[i]}</span>`
         }
 
@@ -48,7 +60,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const stop = (action) => {
         playing = false;
         endTimer();
-        // generateBttn.style.display = "none";
         resetBttn.style.display = "block";
         generatedStringZone.innerHTML = "";
         inputZone.innerHTML = "";
@@ -62,7 +73,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const reset = () => {
         rounds = 1;
-        // generateBttn.style.display = "block";
         resetBttn.style.display = "none";
         roundsElement.textContent = `Round: ${rounds} / ${maxRounds}`;
         remainingTime = maxTime;
@@ -72,7 +82,7 @@ document.addEventListener("DOMContentLoaded", () => {
         startTimer();
     }
 
-    // generateBttn.addEventListener('click', generateString);
+
     resetBttn.addEventListener('click', reset);
 
     document.addEventListener("keydown", (e) => {
@@ -83,6 +93,8 @@ document.addEventListener("DOMContentLoaded", () => {
             inputZone.innerHTML += input;
 
             if (input == code[inputIndex]) {
+                correctSound.currentTime = 0;
+                correctSound.play();
                 document.getElementById("char" + inputIndex).style.color = "#00ff00";
                 inputIndex++;
 
@@ -95,19 +107,22 @@ document.addEventListener("DOMContentLoaded", () => {
                         stop("win");
                     }
                 } else {
-                    document.getElementById("char" + inputIndex).style.color = "yellow";
+                    document.getElementById("char" + inputIndex).style.color = "#ff0";
                 }
             } else {
                 reduceTime(2); // Reduce time by 2 seconds
-                document.getElementById("char" + inputIndex).style.color = "red";
+                errorSound.currentTime = 0;
+                errorSound.play();
+                timerBarElement.style.backgroundColor = "#f00";
+                document.getElementById("char" + inputIndex).style.color = "#f00";
             }
         }
     })
 
     // Timer logic
     const startingCounter = setInterval(() => {
-        timerElement.textContent = Math.ceil(counter / 1000);
         counter -= 1000;
+        timerElement.textContent = Math.ceil(counter / 1000);
         if (counter <= 0) {
             clearInterval(startingCounter);
             startTimer();
@@ -119,8 +134,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const startTimer = () => {
         timer = setInterval(() => {
             timerElement.textContent = Math.ceil(remainingTime / 1000);
-            timerBarElement.style.width = (100 * remainingTime) / maxTime + '%'; 
+            timerBarElement.style.width = (100 * remainingTime) / maxTime + '%';
             remainingTime -= 1000;
+            tickingSound.currentTime = 0;
+            tickingSound.play();
+
+            if (window.getComputedStyle(timerBarElement).getPropertyValue("background-color") == "rgb(255, 0, 0)") {
+                timerBarElement.style.backgroundColor = "#0f0";
+            }
+
             if (remainingTime < 0) {
                 stop("lose");
             }
@@ -145,55 +167,53 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //background
     // geting canvas by Boujjou Achraf
-        var c = document.getElementById("c");
-        var ctx = c.getContext("2d");
+    var c = document.getElementById("c");
+    var ctx = c.getContext("2d");
 
-        //making the canvas full screen
-        c.height = window.innerHeight;
-        c.width = window.innerWidth;
+    //making the canvas full screen
+    c.height = window.innerHeight;
+    c.width = window.innerWidth;
 
-        //chinese characters - taken from the unicode charset
-        var matrix = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}";
-        //converting the string into an array of single characters
-        matrix = matrix.split("");
+    //chinese characters - taken from the unicode charset
+    var matrix = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789@#$%^&*()*&^%+-/~{[|`]}";
+    //converting the string into an array of single characters
+    matrix = matrix.split("");
 
-        var font_size = 10;
-        var columns = c.width/font_size; //number of columns for the rain
-        //an array of drops - one per column
-        var drops = [];
-        //x below is the x coordinate
-        //1 = y co-ordinate of the drop(same for every drop initially)
-        for(var x = 0; x < columns; x++)
-            drops[x] = 1; 
+    var font_size = 10;
+    var columns = c.width / font_size; //number of columns for the rain
+    //an array of drops - one per column
+    var drops = [];
+    //x below is the x coordinate
+    //1 = y co-ordinate of the drop(same for every drop initially)
+    for (var x = 0; x < columns; x++)
+        drops[x] = 1;
 
-        //drawing the characters
-        function draw()
-        {
-            //Black BG for the canvas
-            //translucent BG to show trail
-            ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
-            ctx.fillRect(0, 0, c.width, c.height);
+    //drawing the characters
+    function draw() {
+        //Black BG for the canvas
+        //translucent BG to show trail
+        ctx.fillStyle = "rgba(0, 0, 0, 0.04)";
+        ctx.fillRect(0, 0, c.width, c.height);
 
-            ctx.fillStyle = "#006300";//green text
-            ctx.font = font_size + "px arial";
-            //looping over drops
-            for(var i = 0; i < drops.length; i++)
-            {
-                //a random chinese character to print
-                var text = matrix[Math.floor(Math.random()*matrix.length)];
-                //x = i*font_size, y = value of drops[i]*font_size
-                ctx.fillText(text, i*font_size, drops[i]*font_size);
+        ctx.fillStyle = "#006300";//green text
+        ctx.font = font_size + "px arial";
+        //looping over drops
+        for (var i = 0; i < drops.length; i++) {
+            //a random chinese character to print
+            var text = matrix[Math.floor(Math.random() * matrix.length)];
+            //x = i*font_size, y = value of drops[i]*font_size
+            ctx.fillText(text, i * font_size, drops[i] * font_size);
 
-                //sending the drop back to the top randomly after it has crossed the screen
-                //adding a randomness to the reset to make the drops scattered on the Y axis
-                if(drops[i]*font_size > c.height && Math.random() > 0.975)
-                    drops[i] = 0;
+            //sending the drop back to the top randomly after it has crossed the screen
+            //adding a randomness to the reset to make the drops scattered on the Y axis
+            if (drops[i] * font_size > c.height && Math.random() > 0.975)
+                drops[i] = 0;
 
-                //incrementing Y coordinate
-                drops[i]++;
-            }
+            //incrementing Y coordinate
+            drops[i]++;
         }
+    }
 
-        setInterval(draw, 35);
+    setInterval(draw, 35);
 
 })
