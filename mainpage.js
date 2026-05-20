24eb009427bf65da67de89736c91770a0d121686
N-DaSilva <158066183+N-DaSilva@users.noreply.github.com>
2026-05-20 09:33:58 +0200

    update introduction

--- mainpage.js
+++ mainpage.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const terminalElement = document.getElementById('terminal');
     let terminalLineElement = document.querySelector(".terminal-line.current");
 
-    const textLines = ["db:connect tiktok.com", "Password :", "Connected to Tiktok database", 'search:group -includes "ineskooo_013" & "lapaupau._77" & "arracheusedebrune"', '1 result(s) - "Les Winx <fairy><sparkle>" - last-active '+ yesterdayAtString(), 'cat "Les Winx <fairy><sparkle>', "TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNlY3RldHVyIGFkaXBpc2NpbmcgZWxpdC4gUGhhc2VsbHVzIGV4IGZlbGlzLCBwb3J0YSB2aXRhZSBlbmltIG5lYywgdWx0cmljZXMgdWxsYW1jb3JwZXIgaXBzdW0uIFV0IGVuaW0gc2FwaWVuLCBhbGlxdWV0IGEgYXJjdSBpbiwgdHJpc3RpcXVlIHB1bHZpbmFyIG51bmMuIEZ1c2NlIGFjIGVzdCB2ZWhpY3VsYSwgZGlnbmlzc2ltIHR1cnBpcyBhYywgZnJpbmdpbGxhIGR1aS4gRG9uZWMgYXJjdSB1cm5hLCBjb25zZXF1YXQgdXQgbWkgdmVsLCBmYXVjaWJ1cyBncmF2aWRhIGxpYmVyby4gTWFlY2VuYXMgdXQgdG9ydG9yIHZlbmVuYXRpcywgcG9ydGEgcmlzdXMgdXQsIG1hdHRpcyBtaS4gTnVsbGFtIG5lYyBpcHN1bSBxdWlzIGZlbGlzIHVsdHJpY2llcyBsYWNpbmlhLiBEb25lYyBwaGFyZXRyYSwgc2FwaWVuIGV1IGNvbnZhbGxpcyBydXRydW0sIGFyY3UgdXJuYSBibGFuZGl0IG5pc2ksIG5vbiBzZW1wZXIgZW5pbSBsZWN0dXMgbm9uIG1ldHVzLiBQcm9pbiB0aW5jaWR1bnQgaWFjdWxpcyB2YXJpdXMuIFBlbGxlbnRlc3F1ZSB1dCB0dXJwaXMgZXQgZXJvcyBmZXJtZW50dW0gdmVzdGlidWx1bSBzZWQgdmVsIGxpYmVyby4gTWFlY2VuYXMgdGVtcG9yIGVyYXQgdXQgbGVvIHBvcnR0aXRvciwgcXVpcyBkYXBpYnVzIG51bGxhIHZ1bHB1dGF0ZS4gTmFtIHB1bHZpbmFyIHB1cnVzIG1vbGVzdGllIGR1aSBydXRydW0sIHV0IG1vbGVzdGllIG1pIHJ1dHJ1bS4gVXQgbGFjaW5pYSBtaSBhYyBlcm9zIHNvbGxpY2l0dWRpbiB0ZW1wdXMuIFBoYXNlbGx1cyBhcmN1IGRpYW0sIGZhdWNpYnVzIHZlbCBtYWxlc3VhZGEgaWQsIGVsZW1lbnR1bSBhdCBsaWd1bGEuIEludGVnZXIgZmFjaWxpc2lzIG1vbGVzdGllIGp1c3RvIGFjIHZ1bHB1dGF0ZS4gUHJhZXNlbnQgdHVycGlzIGF1Z3VlLCBzY2VsZXJpc3F1ZSBpZCB0b3J0b3IgaW4sIHZlbmVuYXRpcyBzb2xsaWNpdHVkaW4gZW5pbS4gU2VkIGRhcGlidXMgcG9ydHRpdG9yIGRpYW0gc2l0IGFtZXQgYXVjdG9yLg==", "open Decoder"];
+    const textLines = ["db:connect tiktok.com", "Password :", "Connected to Tiktok database", 'search:group -includes "ineskooo_013" & "lapaupau._77" & "arracheusedebrune"', '1 result(s) - "Les Winx <fairy><sparkle>" - last-active '+ yesterdayAtString(), 'cat "Les Winx <fairy><sparkle>"', "TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNlY3RldHVyIGFkaXBpc2NpbmcgZWxpdC4gUGhhc2VsbHVzIGV4IGZlbGlzLCBwb3J0YSB2aXRhZSBlbmltIG5lYywgdWx0cmljZXMgdWxsYW1jb3JwZXIgaXBzdW0uIFV0IGVuaW0gc2FwaWVuLCBhbGlxdWV0IGEgYXJjdSBpbiwgdHJpc3RpcXVlIHB1bHZpbmFyIG51bmMuIEZ1c2NlIGFjIGVzdCB2ZWhpY3VsYSwgZGlnbmlzc2ltIHR1cnBpcyBhYywgZnJpbmdpbGxhIGR1aS4gRG9uZWMgYXJjdSB1cm5hLCBjb25zZXF1YXQgdXQgbWkgdmVsLCBmYXVjaWJ1cyBncmF2aWRhIGxpYmVyby4gTWFlY2VuYXMgdXQgdG9ydG9yIHZlbmVuYXRpcywgcG9ydGEgcmlzdXMgdXQsIG1hdHRpcyBtaS4gTnVsbGFtIG5lYyBpcHN1bSBxdWlzIGZlbGlzIHVsdHJpY2llcyBsYWNpbmlhLiBEb25lYyBwaGFyZXRyYSwgc2FwaWVuIGV1IGNvbnZhbGxpcyBydXRydW0sIGFyY3UgdXJuYSBibGFuZGl0IG5pc2ksIG5vbiBzZW1wZXIgZW5pbSBsZWN0dXMgbm9uIG1ldHVzLiBQcm9pbiB0aW5jaWR1bnQgaWFjdWxpcyB2YXJpdXMuIFBlbGxlbnRlc3F1ZSB1dCB0dXJwaXMgZXQgZXJvcyBmZXJtZW50dW0gdmVzdGlidWx1bSBzZWQgdmVsIGxpYmVyby4gTWFlY2VuYXMgdGVtcG9yIGVyYXQgdXQgbGVvIHBvcnR0aXRvciwgcXVpcyBkYXBpYnVzIG51bGxhIHZ1bHB1dGF0ZS4gTmFtIHB1bHZpbmFyIHB1cnVzIG1vbGVzdGllIGR1aSBydXRydW0sIHV0IG1vbGVzdGllIG1pIHJ1dHJ1bS4gVXQgbGFjaW5pYSBtaSBhYyBlcm9zIHNvbGxpY2l0dWRpbiB0ZW1wdXMuIFBoYXNlbGx1cyBhcmN1IGRpYW0sIGZhdWNpYnVzIHZlbCBtYWxlc3VhZGEgaWQsIGVsZW1lbnR1bSBhdCBsaWd1bGEuIEludGVnZXIgZmFjaWxpc2lzIG1vbGVzdGllIGp1c3RvIGFjIHZ1bHB1dGF0ZS4gUHJhZXNlbnQgdHVycGlzIGF1Z3VlLCBzY2VsZXJpc3F1ZSBpZCB0b3J0b3IgaW4sIHZlbmVuYXRpcyBzb2xsaWNpdHVkaW4gZW5pbS4gU2VkIGRhcGlidXMgcG9ydHRpdG9yIGRpYW0gc2l0IGFtZXQgYXVjdG9yLg==", "open Decoder",""];
 
     const userInputLinesIndex = [0, 3, 5, 7];
     let currentLineIndex = 0;
@@ -12,6 +12,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let cursorVisible = true;
     let cursorLoop;
+
+    let isPassword = currentLineIndex == 1;
 
     const isInputLine = (lineIndex) => userInputLinesIndex.includes(lineIndex);
 
@@ -55,7 +57,7 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     const revealNextLines = () => {
-        while (currentLineIndex < textLines.length && !isInputLine(currentLineIndex)) {
+        while (currentLineIndex < textLines.length && !isInputLine(currentLineIndex) && !isPassword) {
             appendLine(textLines[currentLineIndex], false);
             currentLineIndex += 1;
         }
@@ -66,6 +68,20 @@ document.addEventListener('DOMContentLoaded', () => {
             renderCurrentLine();
         }
     };
+
+    const inputPassword = () => {
+        appendLine(textLines[currentLineIndex], true);
+        let nbPasswordInputs = 0;
+        document.addEventListener('keydown', (e) => {
+            nbPasswordInputs ++;
+
+            if (nbPasswordInputs == 10){
+                isPassword = false;
+                currentLineIndex++;
+                revealNextLines();
+            }
+        })
+    }
 
     const startCursorLoop = () => {
         if (cursorLoop) {
@@ -89,7 +105,18 @@ document.addEventListener('DOMContentLoaded', () => {
         currentInputText = '';
         cursorVisible = true;
 
+        isPassword = currentLineIndex == 1;
+
         if (currentLineIndex >= textLines.length) {
+            return;
+        }
+
+        if (currentLineIndex == textLines.length -1) {
+            displayPopup();
+        }
+
+        if (isPassword) {
+            inputPassword();
             return;
         }
 
@@ -106,15 +133,6 @@ document.addEventListener('DOMContentLoaded', () => {
         currentCharIndex ++;
     };
 
-    const deleteLastCharacter = () => {
-        if (!isInputLine(currentLineIndex)) {
-            return;
-        }
-
-        currentInputText = currentInputText.slice(0, -1);
-        renderCurrentLine();
-    };
-
     const submitCurrentLine = () => {
         if (!isInputLine(currentLineIndex)) {
             return;
@@ -123,6 +141,12 @@ document.addEventListener('DOMContentLoaded', () => {
         advanceLine();
     };
 
+    const displayPopup = () => {
+        const popup = document.getElementById("popup")
+        popup.classList.remove("hidden");
+        popup.style.display = "block";
+    }
+
     revealNextLines();
     startCursorLoop();
 
@@ -130,12 +154,6 @@ document.addEventListener('DOMContentLoaded', () => {
         if (e.key === 'Enter') {
             e.preventDefault();
             submitCurrentLine();
-            return;
-        }
-
-        if (e.key === 'Backspace') {
-            e.preventDefault();
-            deleteLastCharacter();
             return;
         }
 
